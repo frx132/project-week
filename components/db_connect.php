921ec272dd1d669f7ba9e8381aaff5e0030fc0db
C.K. <you@example.com>
2026-04-29 13:57:08 +0200

    Fix database connection parameters in db_connect.php

--- components/db_connect.php
+++ components/db_connect.php
@@ -1,9 +1,9 @@
 <?php
 // Database connection parameters
-$hostname = "IP Adress"; // this is the hostname that you can find in the PhpMyAdmin and you can write "localhost" too
+$hostname = "127.0.0.1"; // this is the hostname that you can find in the PhpMyAdmin and you can write "localhost" too
 $username = "root"; // be default the userName for the databases is root
 $password = ""; // by default there is no password in the databases
-$dbname = "name of DB"; // here we need to write the Database name
+$dbname = "meal_planner"; // here we need to write the Database name
 
 // create connection, you need to be aware of the order of the parameters
 $connect = new mysqli($hostname, $username, $password, $dbname);
