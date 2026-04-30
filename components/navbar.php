91f35a948a2a869f73a0714dcf93fd48df9d6cce
Kair <kair.versus@gmail.com>
2026-04-30 15:43:36 +0200

    fixed navbar

--- components/navbar.php
+++ components/navbar.php
@@ -11,7 +11,7 @@
             <a class="nav-link active" aria-current="page" href="#">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Features</a>
+            <a class="nav-link" href="#">Recipes</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="../mealPlan/planner.php">Planner</a>
