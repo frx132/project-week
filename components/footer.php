30eebc6fe65b5812db3b657f0fd8a265c4b82a53
Kair <kair.versus@gmail.com>
2026-04-30 15:42:05 +0200

    added footer/navbar component

---
+++ components/footer.php
@@ -0,0 +1,5 @@
+<!-- Footer -->
+<footer class=" text-white text-center py-4 w-100">
+  <p class="mb-0">&copy; 2026 MealPlanner</p>
+</footer>
+<!-- Footer end -->
---
+++ components/navbar.php
@@ -0,0 +1,27 @@
+  <!-- navbar  -->
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Meal Planner</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../mealPlan/planner.php">Planner</a>
+          </li>
+        </ul>
+        <div class="d-flex gap-2">
+          <a href="../functions/login.php" class="btn btn-dark">Login</a>
+          <a href="../functions/register.php" class="btn btn-dark">Sign Up</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <!-- navbar end -->
