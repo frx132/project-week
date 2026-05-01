3b8ad43fa2fee4624a9c935a692977c3faa432f8
okolay <ozgekolay@outlook.com>
2026-05-01 15:40:28 +0200

    delete and edit recipes are completed

--- recipes/delete.php
+++ recipes/delete.php
@@ -1,11 +1,30 @@
 <?php
 session_start();
-// if (!isset($_SESSION['user']) && !isset($_SESSION['admin'])) {
-//     header("Location: ../functions/login.php");
-//     exit;
-// }
-// if (isset($_SESSION['user'])) {
-//     header("Location: ../functions/user_home.php");
-//     exit;
-// }
+if (!isset($_SESSION['user']) && !isset($_SESSION['admin'])) {
+    header("Location: ../functions/login.php");
+    exit;
+}
+
+
 require_once "../components/db_connect.php";
+
+$recipeid = $_GET['recipeid'];
+
+
+$sql_query = "SELECT * FROM `recipes` WHERE id = $recipeid ";
+$result = mysqli_query($connect, $sql_query);
+
+
+if (isset($_GET['recipeid']) && !empty($_GET['recipeid'])) {
+    if (mysqli_num_rows($result) > 0) {
+        $recipe = mysqli_fetch_assoc($result);
+        if ($recipe['recipe_picture']) {
+            unlink("../pictures/{$recipe['recipe_picture']}");
+        }
+        $sql_delete = "DELETE FROM `recipes` WHERE id = $recipeid ";
+        $result_delete = mysqli_query($connect, $sql_delete);
+        header("refresh: 1; url=recipe.php");
+    }
+} else {
+    echo 'Something went wrong';
+}
--- recipes/edit.php
+++ recipes/edit.php
@@ -11,8 +11,14 @@ require_once "../components/db_connect.php";
 $recipeid = $_GET['recipeid'];
 
 
+$sql_query = "SELECT * FROM `recipes` WHERE id = $recipeid ";
+$result = mysqli_query($connect, $sql_query);
+$recipe = mysqli_fetch_assoc($result);
+// var_dump($recipe);
 
-if (isset($_POST['submit'])) {
+
+
+if (isset($_POST['save'])) {
     $title = $_POST['title'];
     $description = $_POST['description'];
     $pictures = fileUpload($_FILES['pictures'], "recipe");
@@ -26,31 +32,30 @@ if (isset($_POST['submit'])) {
     $author = $_SESSION['user'];
 
     if ($_FILES['pictures']['error'] == 4) {
-        $sql_query = "INSERT INTO `recipes`(`title`, `description`, `ingredients`, `instructions`, `prep_time`, `dietary_type`, `category`,`difficulty`, `servings`, `author_id`) VALUES ('$title','$description','$ingredients','$instructions', '$prep_time', '$dietary_type', '$category', '$difficulty', '$servings', '$author' )";
+        $sql_query = "UPDATE recipes SET title = '$title', description = '$description', ingredients = '$ingredients', instructions = '$instructions', prep_time = '$prep_time', dietary_type = '$dietary_type', category = '$category', difficulty = '$difficulty', servings = '$servings' WHERE id = {$recipeid}";
     } else {
-        // var_dump($_FILES['picture']);
-        $picture = fileUpload($_FILES['pictures'], "../pictures");
 
-        $sql_query = "INSERT INTO `recipes`(`title`, `description`, `recipe_picture`, `ingredients`, `instructions`, `prep_time`, `dietary_type`, `category`,`difficulty`, `servings`, `author_id`) VALUES ('$title','$description','$pictures[0]','$ingredients','$instructions', '$prep_time', '$dietary_type', '$category', '$difficulty', '$servings', '$author' )";
-
-        if (isset($pet['picture']) && !empty($pet['picture'])) {
+        if (isset($recipe['pictures']) && !empty($recipe['pictures'])) {
 
             try {
-                unlink("../images/{$pet['picture']}");
+                unlink("../pictures/{$recipe['pictures']}");
             } catch (Exception $e) {
                 echo "Error: can't delete the picture " . $e->getMessage();
             }
         }
+
+        $pictures = fileUpload($_FILES['pictures'], "recipe");
+        $sql_query = "UPDATE recipes SET title = '$title', description = '$description', ingredients = '$ingredients', instructions = '$instructions', prep_time = '$prep_time', dietary_type = '$dietary_type', category = '$category', difficulty = '$difficulty', servings = '$servings', recipe_picture = '$pictures[0]' WHERE id = {$recipeid}";
     }
     $result = mysqli_query($connect, $sql_query);
     if ($result) {
         echo "<div class='alert alert-success'>
-                Congrats, you have added a new recipe!            
+                The recipe is edited successfully!            
                  </div>
         ";
     } else {
         echo "<div class='alert alert-danger'>
-                Something went wrong!
+                Something went wrong! {$pictures[1]}
             </div>
         ";
     }
@@ -66,7 +71,7 @@ if (isset($_POST['submit'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create a recipe</title>
+    <title>Edit recipe</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 
@@ -81,11 +86,11 @@ if (isset($_POST['submit'])) {
                 <form method="POST" enctype="multipart/form-data">
                     <div class="mb-3">
                         <label for="title">Title</label>
-                        <input type="text" class="form-control" id="title" name="title" required>
+                        <input type="text" class="form-control" id="title" name="title" value="<?= $recipe['title'] ?>" required>
                     </div>
                     <div class="mb-3">
                         <label for="description">Description</label>
-                        <textarea class="form-control" id="description" name="description"></textarea>
+                        <textarea class="form-control" id="description" name="description"><?= $recipe['description'] ?></textarea>
                     </div>
                     <div class="mb-3">
                         <label for="pictures">Pictures</label>
@@ -93,11 +98,11 @@ if (isset($_POST['submit'])) {
                     </div>
                     <div class="mb-3">
                         <label for="prep_time">Preparation time (Minutes)</label>
-                        <input type="number" class="form-control" id="prep_time" name="prep_time" required>
+                        <input type="number" class="form-control" id="prep_time" name="prep_time" value="<?= $recipe['prep_time'] ?>" required>
                     </div>
                     <div class="mb-3">
                         <label for="servings">Servings</label>
-                        <input type="number" class="form-control" id="servings" name="servings">
+                        <input type="number" class="form-control" id="servings" name="servings" value="<?= $recipe['servings'] ?>">
                     </div>
 
 
@@ -105,9 +110,9 @@ if (isset($_POST['submit'])) {
                         <label for="dietary_type">Dietary type</label>
                         <select name="dietary_type" id="dietary_type" class="form-select" required>
                             <option value="null">Please select an option</option>
-                            <option value="Vegan">Vegan</option>
-                            <option value="Vegeterian">Vegerterian</option>
-                            <option value="Non-vegeterian">Non-vegeterian</option>
+                            <option value="Vegeterian" <?= (isset($recipe["dietary_type"]) && $recipe["dietary_type"] == 'Vegeterian') ? 'selected' : '' ?>>Vegeterian</option>
+                            <option value="Non-vegeterian" <?= (isset($recipe["dietary_type"]) && $recipe["dietary_type"] == 'Non-vegeterian') ? 'selected' : '' ?>>Non-vegeterian</option>
+                            <option value="Vegan" <?= (isset($recipe["dietary_type"]) && $recipe["dietary_type"] == 'Vegan') ? 'selected' : '' ?>>Vegan</option>
                         </select>
 
                     </div>
@@ -115,9 +120,9 @@ if (isset($_POST['submit'])) {
                         <label for="difficulty">Difficulty</label>
                         <select name="difficulty" id="difficulty" class="form-select">
                             <option value="null">Please select an option</option>
-                            <option value="Easy">Easy</option>
-                            <option value="Medium">Medium</option>
-                            <option value="Complicated">Complicated</option>
+                            <option value="Easy" <?= (isset($recipe["difficulty"]) && $recipe["difficulty"] == 'Easy') ? 'selected' : '' ?>>Easy</option>
+                            <option value="Medium" <?= (isset($recipe["difficulty"]) && $recipe["difficulty"] == 'Medium') ? 'selected' : '' ?>>Medium</option>
+                            <option value="Complicated" <?= (isset($recipe["difficulty"]) && $recipe["difficulty"] == 'Complicated') ? 'selected' : '' ?>>Complicated</option>
                         </select>
 
                     </div>
@@ -125,23 +130,24 @@ if (isset($_POST['submit'])) {
                         <label for="category" class="form-label">Category</label>
                         <select class="form-control" id="category" name="category">
                             <option value="null">Please select an option</option>
-                            <option value="Chicken meals">Chicken meals</option>
-                            <option value="Vegetables">Vegetables</option>
-                            <option value="Desserts">Desserts</option>
-                            <option value="Fish meals">Fish meals</option>
+                            <option value="Chicken meals" <?= (isset($recipe["category"]) && $recipe["category"] == 'Chicken meals') ? 'selected' : '' ?>>Chicken meals</option>
+                            <option value="Vegetables" <?= (isset($recipe["category"]) && $recipe["category"] == 'Vegetables') ? 'selected' : '' ?>>Vegetables</option>
+                            <option value="Desserts" <?= (isset($recipe["category"]) && $recipe["category"] == 'Desserts') ? 'selected' : '' ?>>Desserts</option>
+                            <option value="Fish meals" <?= (isset($recipe["category"]) && $recipe["category"] == 'Fish meals') ? 'selected' : '' ?>>Fish meals</option>
+                        </select>
                         </select>
                     </div>
 
                     <div class="mb-3">
                         <label for="ingredients">Ingredients</label>
-                        <textarea class="form-control" id="ingredients" name="ingredients" required></textarea>
+                        <textarea class="form-control" id="ingredients" name="ingredients" required><?= $recipe['ingredients'] ?></textarea>
                     </div>
                     <div class="mb-3">
                         <label for="instructions">Instructions</label>
-                        <textarea class="form-control" id="instructions" name="instructions" required></textarea>
+                        <textarea class="form-control" id="instructions" name="instructions" required><?= $recipe['instructions'] ?></textarea>
                     </div>
 
-                    <input type="submit" class="btn btn-primary" name="submit" value="Submit">
+                    <input type="save" class="btn btn-primary" name="save" value="Save">
                 </form>
             </div>
         </div>
